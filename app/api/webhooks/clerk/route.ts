b449e994ac5819829efe21237732cf7a30c1f1f6
AlgoFoe <varun.singh10011@gmail.com>
2025-01-17 23:06:43 +0530

    fixed role key in public metadata

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -77,6 +77,7 @@ export async function POST(req: Request) {
       await clerkClient.users.updateUserMetadata(id, {
         publicMetadata: {
           userId: newUser._id,
+          role:"patient"
         },
       });
     }
